b184e2b5a4cdca2483870971500f8a29abf890f8
SuhdudeDX <puregubug888@gmail.com>
2022-11-16 21:52:58 +0700

    import libralies

--- Personal_financial_cal.py
+++ Personal_financial_cal.py
@@ -7,3 +7,24 @@ class Life:
         self.income = 1000
         self.tax_rate = 0.19
         self.spending = 650
+
+    def earn(self, t):
+        return 12 * self.income
+
+    def spend(self, t):
+        return 12 * self.spending
+
+    def pay_taxes(self, t):
+        return self.earn(t) * self.tax_rate
+    
+def live_without_investing(x, t, you):
+    return you.earn(t) - you.spend(t) - you.pay_taxes(t)
+
+def simulate(you):
+    t = np.linspace(0, 100, num=101)
+    x = odeint(live_without_investing, 0, t, args=(you,))
+    return pd.DataFrame({'time': t, 'wallet (non-investor)': x})
+
+
+you = Life()
+df = simulate(you)
